1102a99dd389d7f2f7c40dc5cfdfcecd6694e913
348048638 <348048638@qq.com>
2021-09-29 14:40:02 +0800

    'nothing'

--- src/main/java/a/demo/server/aspect/TheAspect.java
+++ src/main/java/a/demo/server/aspect/TheAspect.java
@@ -19,9 +19,9 @@ public class TheAspect {
     public void Before(JoinPoint joinPoint){
         System.err.println("Before");
         Signature signature=joinPoint.getSignature();
-        System.out.println(signature);
-        System.out.println(signature.getName());
-        System.out.println(Arrays.asList(joinPoint.getArgs()));
+        System.out.println("Before-----"+signature);
+        System.out.println("Before-----"+signature.getName());
+        System.out.println("Before-----"+Arrays.asList(joinPoint.getArgs()));
         System.err.println("Before... ...");
     }
     @After(value = "Pointcut()")
@@ -32,31 +32,32 @@ public class TheAspect {
     @AfterReturning(value = "Pointcut()",returning = "o")
     public void AfterReturning(Object o){
         System.err.println("AfterReturning");
-        System.out.println(o);
+        System.out.println("AfterReturning-----"+o);
         System.err.println("AfterReturning... ...");
     }
     @AfterThrowing(value = "Pointcut()",throwing = "e")
     public void AfterThrowing(Exception e){
         System.err.println("AfterThrowing");
-        System.out.println(e);
+        System.out.println("AfterThrowing-----"+e);
         System.err.println("AfterThrowing... ...");
     }
     @Around(value = "execution(public * a.demo.server.module.controller.*.*(..))")
     public Object Around(ProceedingJoinPoint proceedingJoinPoint){
         System.err.println("Around");
-        System.out.println(proceedingJoinPoint.getSignature().getName());
+        System.out.println("Around-----"+proceedingJoinPoint.getSignature().getName());
         try {
             Object[]args=proceedingJoinPoint.getArgs();
-            if (args!=null&&args.length>1){
-                System.out.println(args[0]);
+            if (args!=null&&args.length>0){
+                System.out.println("Around-----"+args[0]);
                 for (int i=0;i<args.length;i++){
                     if (args[i] instanceof String&&args[i]==null){
                         args[i]="";
                     }
                 }
-                System.out.println(args[0]);
+                System.out.println("Around-----"+args[0]);
                 Object o=proceedingJoinPoint.proceed(args);
-                if (o!=null&&o instanceof TheResult)
+                System.out.println("Around-----"+o);
+                if (o!=null&&o instanceof String)
                     return o;
                 return "type exception";
             }
